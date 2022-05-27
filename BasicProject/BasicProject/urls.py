86f6c74bb190b806cd5ca7498646de3b899e123d
Saad Shahzad <s9shahza@uwaterloo.ca>
2022-05-27 15:07:01 -0400

    Create initial Django app

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -24,3 +24,11 @@ urlpatterns = [
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
 ]
+from django.conf.urls import include, url
+import HelloDjangoApp.views
+
+# Django processes URL patterns in the order they appear in the array
+urlpatterns = [
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+]
